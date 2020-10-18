070cbc077d4e559600a2cf05077381613e481d97
Alex <aliaksei.tokarev@gmail.com>
2020-10-19 00:35:15 +0300

    feat: add check f.brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let count = [];
+    let bracket = bracketsConfig.flat().join('');
+    for (let i = 0; i < str.length; i++) {
+        let index = count.findIndex(el => str[i] === el);
+        let configIndex = bracket.indexOf(str[i]);
+        // if (configIndex !== bracket.lastIndexOf(str[i]))
+        if (index >= 0 && configIndex % 2 === 0) {
+            count[index + 1] += 1
+        } else if (index < 0 && configIndex % 2 === 0) {
+            count[count.length] = str[i];
+            count[count.length] = 1;
+        }
+        
+        if (configIndex % 2 !== 0) {
+            openIndex = count.findIndex(el => bracket[configIndex-1] === el);
+            count[openIndex + 1] -= 1;
+              if (count[openIndex + 1] < 0) return false;
+        }   
+        
+    }
+    for (let index = 1; index < count.length; index +=2) {
+        if (count[index] > 0) return false;
+        
+    }
+    return true;
 }
+
+// check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]);
